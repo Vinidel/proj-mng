57c3890a1836d5905ac7e6d2667d2ccd4ef21c79
Vinicius Delascio Fortunato <vinidroid@gmail.com>
2019-11-05 20:54:25 +1100

    master add connection check

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 const http = require('http');
 const dotenv = require('dotenv');
 const app = require('./app');
+const db = require('./database/models');
 
 dotenv.config();
 const port = process.env.PORT || '8081';
 const server = http.createServer(app);
 
-server.listen(port, () => {
-  console.log(`Server running in port ${port}`);
-});
+db.sequelize.authenticate()
+  .then(() => server.listen(port, () => {
+    console.log(`Server running in port ${port}`);
+  }))
+  .catch((error) => console.log('Cannot start sever', error));
